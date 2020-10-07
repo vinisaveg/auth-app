f127e63bf9895f98807c700f3eaca2ac8881221c
Saveg <savegdesigner@gmail.com>
2020-10-07 09:08:10 -0300

    adding urql, setting up graphql url, testing urql query :zap:

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -2,14 +2,19 @@ import { ChakraProvider } from "@chakra-ui/core";
 
 import theme from "../theme";
 import { AppProps } from "next/app";
+import { Provider } from "urql";
+
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { client } from "../utils/urql";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <DarkModeSwitch />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <DarkModeSwitch />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   );
 }
 
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,12 +1,50 @@
-import { Box } from "@chakra-ui/core";
+import { Button, ButtonGroup, Flex, Heading } from "@chakra-ui/core";
+import Link from "next/link";
+import { useQuery } from "urql";
+
 import { Container } from "../components/Container";
 
-const Index = () => (
-  <Container height="100vh">
-    <Box>
-      <h1>Hello!</h1>
-    </Box>
-  </Container>
-);
+const UsersQuery = `
+    query {
+      users {
+        id
+        username
+        createdAt
+        updatedAt
+      }
+    }
+`;
+
+const Index = () => {
+  const [usersResult, reexecuteQuery] = useQuery({
+    query: UsersQuery,
+  });
+
+  const { data } = usersResult;
+
+  console.log(data);
+
+  return (
+    <Container height="100vh">
+      <Flex flexDir="column" alignItems="center" justifyContent="center">
+        <Heading mb={3}>Hello!</Heading>
+
+        <ButtonGroup spacing={4}>
+          <Link href="/login">
+            <Button variant="solid" size="md">
+              Login
+            </Button>
+          </Link>
+
+          <Link href="/register">
+            <Button variant="solid" size="md">
+              Register
+            </Button>
+          </Link>
+        </ButtonGroup>
+      </Flex>
+    </Container>
+  );
+};
 
 export default Index;
